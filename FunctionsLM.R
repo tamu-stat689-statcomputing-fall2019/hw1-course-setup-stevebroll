cc9ed62a5d1b61ce7318c61f44128382d6ae9d3e
stevebroll <stevebroll@tamu.edu>
2019-09-03 13:12:51 -0500

    add set seed to generateY; reformat comments to reduce length per line

--- FunctionsLM.R
+++ FunctionsLM.R
@@ -1,10 +1,13 @@
-# Generate n-dimensional response Y that follows linear regression model Y = Xbeta + epsilon, where epsilon is normal zero with variance sigma^2 independent across samples. Seed should be set at the beginning of the function
+# Generate n-dimensional response Y that follows linear regression model
+# Y = Xbeta + epsilon, where epsilon is normal zero with variance sigma^2 
+# independent across samples. Seed should be set at the beginning of the function
 # X - design matrix
 # beta - given parameter vector
 # sigma - standard deviation of the noise
 # seed  - starting seed value
 generateY <- function(X, beta, sigma, seed = 5832652){
   #[ToDo] Set seed and generate Y following linear model
+  set.seed(seed)
   
   # Return Y
   return(Y)
