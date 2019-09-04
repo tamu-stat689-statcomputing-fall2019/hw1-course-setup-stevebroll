61895dadf56d74f16ac02abbf2b26f53ed622b6e
stevebroll <stevebroll@tamu.edu>
2019-09-04 10:51:40 -0500

    use functions from FunctionsLM.R to generate Y and calculate beta_LS with MSE

--- SimpleProgram.R
+++ SimpleProgram.R
@@ -9,7 +9,10 @@ beta = rep(2,p) # true vector of coefficients
 X = matrix(rnorm(n*p), n, p) # n by p matrix of predictors
 
 # [ToDo] Use generateY function to generate Y
+Y = generateY(X, beta, sigma) # store generated Y value
 
 # [ToDo] Use calculateBeta function to calculate beta_LS
+beta_LS = calculateBeta(X,Y) # use generated Y to calculate beta_LS
 
 # [ToDo] Use calculateMSE to assess the estimation error measured by squared eucledian distance - ||beta - beta_LS||_2^2
+MSE = calculateMSE(beta, beta_LS) # use generated beta_LS to calculate MSE
